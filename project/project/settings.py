88561425663e33e44cd3ad7e25a2cc61e9eef81d
arvind <arvindsain005@gamil.com>
2025-04-11 06:45:49 +0530

    removed extra  code.

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'accounts',
     'qa',
 ]
@@ -125,7 +124,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
